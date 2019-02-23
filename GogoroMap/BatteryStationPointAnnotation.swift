85d9c2a87a027756f9644e93eb5a542b10ba6cec
TerryCK <vul354@gmail.com>
2019-02-23 15:33:40 +0800

    Refactoring BatteryStationPointAnnotation

---
+++ GogoroMap/BatteryStationPointAnnotation.swift
@@ -0,0 +1,33 @@
+//
+//  BatteryStationPointAnnotation.swift
+//  GogoroMap
+//
+//  Created by 陳 冠禎 on 23/02/2019.
+//  Copyright © 2019 陳 冠禎. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+final class BatteryStationPointAnnotation: MKPointAnnotation {
+    let image: UIImage,
+    placemark: MKPlacemark,
+    checkinCounter: Int,
+    checkinDay, address: String,
+    isOpening: Bool
+    
+    init(station: StationDataSorce) {
+        let name     = station.name.localized() ?? ""
+        let location = CLLocationCoordinate2D(latitude: station.latitude, longitude: station.longitude)
+        placemark    = MKPlacemark(coordinate: location, addressDictionary: [name: ""])
+        image        = station.annotationImage
+        address      = station.address.localized() ?? ""
+        isOpening    = station.isOpening
+        checkinDay   = station.checkinDay
+        checkinCounter = station.checkinCounter
+        super.init()
+        title      = name
+        subtitle   = "\("Open hours:".localize()) \(station.availableTime ?? "")"
+        coordinate = location
+    }
+}
