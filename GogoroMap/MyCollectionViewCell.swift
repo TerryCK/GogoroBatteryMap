f826100b30b48d4717d7edaa7ff0d6bbb4722f48
TerryCK <vul354@gmail.com>
2019-02-23 13:46:34 +0800

    Remove redundant code

--- GogoroMap/MyCollectionViewCell.swift
+++ GogoroMap/MyCollectionViewCell.swift
@@ -6,9 +6,7 @@
 //  Copyright © 2017 陳 冠禎. All rights reserved.
 //
 
-import Foundation
 import UIKit
-
 
 final class MyCollectionViewCell: BaseCollectionViewCell {
     
@@ -58,35 +56,6 @@ final class MyCollectionViewCell: BaseCollectionViewCell {
         subStackView.anchor(top: titleLabel.bottomAnchor, left: imageView.rightAnchor, bottom: viewContainer.bottomAnchor, right: viewContainer.rightAnchor, topPadding: 5, leftPadding: 12, bottomPadding: 12, rightPadding: 6, width: 0, height: 20)
         
     }
-    
-//    override lazy var viewContainer: UIView = {
-//        let containerView = UIView()
-//        let blurEffect = UIBlurEffect(style: .extraLight)
-//        let blurEffectView = UIVisualEffectView(effect: blurEffect)
-//        blurEffectView.frame = self.bounds
-//        blurEffectView.alpha = 0.87
-//
-//        self.addSubview(blurEffectView)
-//
-//        let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
-//        let vibrancyEffectView = UIVisualEffectView(effect: vibrancyEffect)
-//
-//        vibrancyEffectView.frame = self.bounds
-//        blurEffectView.contentView.addSubview(vibrancyEffectView)
-//
-//        let vibrancyEffectContentView = vibrancyEffectView.contentView
-//        vibrancyEffectContentView.addSubview(containerView)
-//
-//        var buttomAnchor = vibrancyEffectContentView.bottomAnchor
-//        if #available(iOS 11.0, *), UIDevice.isiPhoneX {
-//            let safeAreaBottom: NSLayoutYAxisAnchor = vibrancyEffectContentView.safeAreaLayoutGuide.bottomAnchor
-//            buttomAnchor = safeAreaBottom
-//        }
-//
-//        containerView.anchor(top: vibrancyEffectContentView.topAnchor, left: vibrancyEffectContentView.leftAnchor, bottom: buttomAnchor, right:  vibrancyEffectContentView.rightAnchor)
-//
-//        return containerView
-//    }()
 }
 
 
--- GogoroMap/String+Extension.swift
+++ GogoroMap/String+Extension.swift
@@ -9,7 +9,5 @@
 import Foundation
 
 extension String {
-    func localize() -> String {
-        return NSLocalizedString(self, comment: "")
-    }
+    func localize() -> String { return NSLocalizedString(self, comment: "") }
 }
