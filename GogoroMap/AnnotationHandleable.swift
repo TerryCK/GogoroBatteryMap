f71df03cfe48818500db13d0fd97b00d0afb9c80
TerryCK <vul354@gmail.com>
2019-02-24 22:23:36 +0800

    - Refactoring

--- GogoroMap/AnnotationHandleable.swift
+++ GogoroMap/AnnotationHandleable.swift
@@ -1,96 +1,97 @@
+////
+////  AnnotationHandleable.swift
+////  GogoroMap
+////
+////  Created by 陳 冠禎 on 2017/8/12.
+////  Copyright © 2017年 陳 冠禎. All rights reserved.
+////
 //
-//  AnnotationHandleable.swift
-//  GogoroMap
 //
-//  Created by 陳 冠禎 on 2017/8/12.
-//  Copyright © 2017年 陳 冠禎. All rights reserved.
+//import MapKit
+//import Foundation
+//import CoreLocation
+//import CloudKit
 //
-
-
-import MapKit
-import Foundation
-import CoreLocation
-import CloudKit
-
-protocol AnnotationHandleable {
-    func getObjectArray(from stations: [Station], userLocation: CLLocation) -> [CustomPointAnnotation]
-}
-
-extension AnnotationHandleable {
-    
-    func getObjectArray(from stations: [Station], userLocation: CLLocation) -> [CustomPointAnnotation] {
-        return stations.map { (station) -> CustomPointAnnotation in
-            let isEnglish = NSLocale.preferredLanguages[0] == "en"
-            
-            let latitude: CLLocationDegrees = station.latitude ?? 0.0
-            let longitude: CLLocationDegrees = station.longitude ?? 0.0
-            let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-            
-            let twName = station.locName?.twName ?? ""
-            let engName = station.locName?.engName ?? ""
-            
-            let twAddress = station.address?.twName ?? ""
-            let engAddress = station.address?.engName ?? ""
-            
-            let address = isEnglish ? engAddress : twAddress
-            let title = isEnglish ? engName : twName
-            
-            return CustomPointAnnotation(title: title,
-                                         subtitle: "\("Open hours:".localize()) \(station.availableTime ?? "")",
-                                         coordinate: location,
-                                         placemark: MKPlacemark(coordinate: location, addressDictionary: [title: ""]),
-                                         image: getImage(with: station),
-                                         address: address,
-                                         isOpening: station.state == 1
-            )
-        }
-    }
-    
-    
-    typealias StationName = String
-    
-    func getImage(with name: StationName?) -> UIImage {
-        
-        let convenientKeywords = ["HiLife", "全聯", "7-ELEVEN", "全家"]
-        let mallKeywords = ["家樂福", "大潤發", "Mall", "百貨"]
-        let gasStationKeyword = ["加油"]
-        let goStationKeyword = ["Gogoro"]
-        
-        let closure = { (result: Bool, keyword: String) -> Bool in
-            return result || name?.contains(keyword) ?? false
-        }
-        
-        let isConvenientStore = convenientKeywords.reduce(false, closure)
-        let isMall = mallKeywords.reduce(false, closure)
-        let isGasStation = gasStationKeyword.reduce(false, closure)
-        let isGoStation = goStationKeyword.reduce(false, closure)
-        
-        
-        return isConvenientStore ? #imageLiteral(resourceName: "convenientStore") : isMall ? #imageLiteral(resourceName: "mallStore") : isGasStation ? #imageLiteral(resourceName: "gasStation") : isGoStation ? #imageLiteral(resourceName: "goStore") : #imageLiteral(resourceName: "pinFull")
-    }
-    
-     func getImage(with annotation: CustomPointAnnotation) -> UIImage {
-        return !annotation.isOpening ? #imageLiteral(resourceName: "building") : getImage(with: annotation.title)
-    }
-    
-    private func getImage(with station: Station) -> UIImage {
-        return station.state != 1 ? #imageLiteral(resourceName: "building") : getImage(with: station.locName?.twName)
-    }
-    
-    func updataAnnotationImage(annotations: [CustomPointAnnotation]) {
-        annotations.forEach { (element) in
-            element.image = getImage(with: element)
-        }
-    }
-}
-
-extension Collection where Element: CustomPointAnnotation {
-    var  toRecordValue: CKRecordValue {
-        return toData.toRecordValue
-    }
-    var toData: Data {
-        return NSKeyedArchiver.archivedData(withRootObject: self)
-    }
-}
-
-
+//protocol AnnotationHandleable {
+//    func getObjectArray(from stations: [Station], userLocation: CLLocation) -> [CustomPointAnnotation]
+//}
+//
+//extension AnnotationHandleable {
+//    
+//    func getObjectArray(from stations: [Station], userLocation: CLLocation) -> [CustomPointAnnotation] {
+//        return stations.map { (station) -> CustomPointAnnotation in
+//            let isEnglish = NSLocale.preferredLanguages[0] == "en"
+//            
+//            let latitude: CLLocationDegrees = station.latitude ?? 0.0
+//            let longitude: CLLocationDegrees = station.longitude ?? 0.0
+//            let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//            
+//            let twName = station.locName?.twName ?? ""
+//            let engName = station.locName?.engName ?? ""
+//            
+//            let twAddress = station.address?.twName ?? ""
+//            let engAddress = station.address?.engName ?? ""
+//            
+//            let address = isEnglish ? engAddress : twAddress
+//            let title = isEnglish ? engName : twName
+//            
+//            return CustomPointAnnotation(title: title,
+//                                         subtitle: "\("Open hours:".localize()) \(station.availableTime ?? "")",
+//                                         coordinate: location,
+//                                         placemark: MKPlacemark(coordinate: location, addressDictionary: [title: ""]),
+//                                         image: getImage(with: station),
+//                                         address: address,
+//                                         isOpening: station.state == 1
+//            )
+//        }
+//    }
+//    
+//
+//    
+//    typealias StationName = String
+//    
+////    func getImage(with name: StationName?) -> UIImage {
+////
+////        let convenientKeywords = ["HiLife", "全聯", "7-ELEVEN", "全家"]
+////        let mallKeywords = ["家樂福", "大潤發", "Mall", "百貨"]
+////        let gasStationKeyword = ["加油"]
+////        let goStationKeyword = ["Gogoro"]
+////
+////        let closure = { (result: Bool, keyword: String) -> Bool in
+////            return result || name?.contains(keyword) ?? false
+////        }
+////
+////        let isConvenientStore = convenientKeywords.reduce(false, closure)
+////        let isMall = mallKeywords.reduce(false, closure)
+////        let isGasStation = gasStationKeyword.reduce(false, closure)
+////        let isGoStation = goStationKeyword.reduce(false, closure)
+////
+////
+////        return isConvenientStore ? #imageLiteral(resourceName: "convenientStore") : isMall ? #imageLiteral(resourceName: "mallStore") : isGasStation ? #imageLiteral(resourceName: "gasStation") : isGoStation ? #imageLiteral(resourceName: "goStore") : #imageLiteral(resourceName: "pinFull")
+////    }
+////
+////     func getImage(with annotation: CustomPointAnnotation) -> UIImage {
+////        return !annotation.isOpening ? #imageLiteral(resourceName: "building") : getImage(with: annotation.title)
+////    }
+////
+////    private func getImage(with station: Station) -> UIImage {
+////        return station.state != 1 ? #imageLiteral(resourceName: "building") : getImage(with: station.locName?.twName)
+////    }
+//    
+//    func updataAnnotationImage(annotations: [CustomPointAnnotation]) {
+//        annotations.forEach { (element) in
+//            element.image = getImage(with: element)
+//        }
+//    }
+//}
+//
+//extension Collection where Element: CustomPointAnnotation {
+//    var  toRecordValue: CKRecordValue {
+//        return toData.toRecordValue
+//    }
+//    var toData: Data {
+//        return NSKeyedArchiver.archivedData(withRootObject: self)
+//    }
+//}
+//
+//
--- GogoroMap/Parsable.swift
+++ GogoroMap/Parsable.swift
@@ -6,24 +6,24 @@
 //  Copyright © 2017 陳 冠禎. All rights reserved.
 //
 
-import Foundation
-
-protocol Parsable {
-    var parsed: [CustomPointAnnotation]? { get }
-}
-
-extension Parsable {
-    var parsed: [CustomPointAnnotation]? {
-        let data: Data = self as? Data ?? Data()
-        guard
-            let jsonDictionary = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
-            let jsonDic = jsonDictionary?["data"] as? [[String: Any]] else {
-                return nil
-        }
-        return jsonDic.map(Station.init(dictionary:)).customPointAnnotations
-    }
-}
-
-extension Data: Parsable { }
-extension NSData: Parsable { }
-
+//import Foundation
+//
+//protocol Parsable {
+//    var parsed: [CustomPointAnnotation]? { get }
+//}
+//
+//extension Parsable {
+//    var parsed: [CustomPointAnnotation]? {
+//        let data: Data = self as? Data ?? Data()
+//        guard
+//            let jsonDictionary = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+//            let jsonDic = jsonDictionary?["data"] as? [[String: Any]] else {
+//                return nil
+//        }
+//        return jsonDic.map(Station.init(dictionary:)).customPointAnnotations
+//    }
+//}
+//
+//extension Data: Parsable { }
+//extension NSData: Parsable { }
+//
