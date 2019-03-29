9abceabafb07231841035c662940513819c47b84
TerryCK <vul354@gmail.com>
2019-03-30 00:18:51 +0800

    Revise battery station operating condition

--- GogoroMap/BatteryStationPointAnnotation.swift
+++ GogoroMap/BatteryStationPointAnnotation.swift
@@ -10,17 +10,6 @@ import UIKit
 import MapKit
 
 extension BatteryStationPointAnnotation {
-    public var iconImage: UIImage {
-        guard checkinCounter ?? 0 <= 0 else { return #imageLiteral(resourceName: "checkin") }
-        guard let name = title, state == 1 else { return #imageLiteral(resourceName: "building") }
-        if name.contains("Gogoro")                                { return #imageLiteral(resourceName: "goStore") }
-        if ["加油", "中油"].reduce(false, { $0 || name.contains($1) })   { return #imageLiteral(resourceName: "gasStation") }
-        if ["家樂福", "大潤發", "Mall", "百貨", "Global Mall", "CITYLINK"].reduce(false, { $0 || name.contains($1) })     { return #imageLiteral(resourceName: "mallStore") }
-        if ["HiLife", "全聯", "7-ELEVEN", "全家"].reduce(false, { $0 || name.contains($1) })  { return #imageLiteral(resourceName: "convenientStore") }
-        return #imageLiteral(resourceName: "pinFull")
-    }
-    
-    
     convenience init(_ customPointAnnotation: CustomPointAnnotation) {
         self.init(title         : customPointAnnotation.title,
                   subtitle      : customPointAnnotation.subtitle,
@@ -32,6 +21,19 @@ extension BatteryStationPointAnnotation {
     } 
 }
 
+extension BatteryDataModal {
+    var isOperating: Bool { return state == 1 && !(title?.contains("(維修中)") ?? true) }
+    
+    public var iconImage: UIImage {
+        guard checkinCounter ?? 0 <= 0 else { return #imageLiteral(resourceName: "checkin") }
+        guard let name = title, isOperating else { return #imageLiteral(resourceName: "building") }
+        if name.contains("Gogoro")                                { return #imageLiteral(resourceName: "goStore") }
+        if ["加油", "中油"].reduce(false, { $0 || name.contains($1) })   { return #imageLiteral(resourceName: "gasStation") }
+        if ["家樂福", "大潤發", "Mall", "百貨", "Global Mall", "CITYLINK"].reduce(false, { $0 || name.contains($1) })     { return #imageLiteral(resourceName: "mallStore") }
+        if ["HiLife", "全聯", "7-ELEVEN", "全家"].reduce(false, { $0 || name.contains($1) })  { return #imageLiteral(resourceName: "convenientStore") }
+        return #imageLiteral(resourceName: "pinFull")
+    }
+}
 protocol BatteryDataModal {
     var title: String? { get }
     var subtitle: String? { get }
@@ -41,6 +43,7 @@ protocol BatteryDataModal {
     var checkinCounter: Int? { set get }
     var checkinDay: String? { set get }
     var iconImage: UIImage { get }
+    var isOperating: Bool { get }
     func distance(from userPosition: CLLocation) -> Double
 }
 
