814ad56d31383552086b650878c0b8758373cf8d
TerryCK <vul354@gmail.com>
2019-04-08 21:56:02 +0800

    Refactoring

--- GogoroMap/AppDelegate.swift
+++ GogoroMap/AppDelegate.swift
@@ -23,7 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         
         setupIAPOberserver()
-        
 
         #if RELEASE
         FirebaseApp.configure()
