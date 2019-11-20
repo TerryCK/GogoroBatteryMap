6e7ee0de11ac0b0c1400d4108cfb3f89e2659336
Terry Chen <>
2019-11-20 18:31:35 +0800

    Improvement fetch remote data timing

--- GogoroMap/AppDelegate.swift
+++ GogoroMap/AppDelegate.swift
@@ -57,11 +57,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
+        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
+        DataManager.shared.fetchStations()
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
     
