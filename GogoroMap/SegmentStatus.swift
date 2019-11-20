d4c3557fa9cf913029a7c05f477a6fb46a9eb184
Terry Chen <vul354@gmail.com>
2019-11-20 15:02:53 +0800

    Refactor

--- GogoroMap/SegmentStatus.swift
+++ GogoroMap/SegmentStatus.swift
@@ -8,43 +8,70 @@
 
 import UIKit
 
-enum SegmentStatus: Int, CaseIterable {
+
+enum TabItemCase: Int, CaseIterable {
     
-    case nearby = 0, checkin, uncheck, building
+    case nearby, checkin, uncheck, building, setting, backup
     
-    
-    var name: String {
+    var title: String {
         switch self {
-        case .building  : return "即將啟用"
-        case .nearby    : return "附近營運中"
+        case .nearby    : return "營運中"
         case .checkin   : return "已打卡"
         case .uncheck   : return "未打卡"
+        case .building  : return "即將啟用"
+        case .setting   : return "設定"
+        case .backup    : return "雲端備份"
         }
     }
     
-    var eventName: String {
-        return String(describing: self)
+    
+    var normalImage: UIImage {
+        switch self {
+        case .nearby:    return #imageLiteral(resourceName: "recent")
+        case .checkin:   return #imageLiteral(resourceName: "checkin")
+        case .uncheck:   return #imageLiteral(resourceName: "pinFull")
+        case .building:  return #imageLiteral(resourceName: "building")
+        case .setting:   return #imageLiteral(resourceName: "setting")
+        case .backup:    return #imageLiteral(resourceName: "cloud")
+        }
     }
+    
+    static let viewControllers = TabItemCase.allCases.map { $0.viewController }
     
     var hanlder: (BatteryStationPointAnnotation) -> Bool {
         switch self {
         case .checkin       : return { $0.checkinCounter ?? 0 > 0 && $0.isOperating }
         case .uncheck       : return { $0.checkinCounter ?? 0 == 0 && $0.isOperating }
-        case .nearby        : return { $0.isOperating }
+        case .nearby,.setting, .backup        : return { $0.isOperating }
         case .building      : return { !$0.isOperating }
+        }
+    }
+    
+    private var viewController: UIViewController {
+        switch self {
+            
+        case .building, .nearby , .uncheck, .checkin:
+            return TableViewController(style: .grouped)
+            
+        case .setting   :
+            let flowLyout: UICollectionViewFlowLayout = {
+                $0.itemSize = CGSize(width: UIScreen.main.bounds.width - 20 , height: UIScreen.main.bounds.height - 90)
+                $0.minimumLineSpacing = 0
+                $0.minimumInteritemSpacing = 0
+                return $0
+            }(UICollectionViewFlowLayout())
+            return MenuController(collectionViewLayout: flowLyout)
+        case .backup: return BackupViewController(style: .grouped)
         }
     }
     
     var stationDataSource: [BatteryStationPointAnnotation] {
         switch self {
-        case .nearby:
-            return DataManager.shared.stations
-        case .checkin:
-            return DataManager.shared.checkins
-        case .uncheck:
-            return DataManager.shared.unchecks
-        case .building:
-            return DataManager.shared.buildings
+        case .nearby            : return DataManager.shared.stations
+        case .checkin, .backup  : return DataManager.shared.checkins
+        case .uncheck           : return DataManager.shared.unchecks
+        case .building          : return DataManager.shared.buildings
+        case .setting           : return DataManager.shared.originalStations
         }
     }
 }
